5cab27b10e5dc3f805d66ee44cc6442c82fcc131
thejsj <jorge.silva@thejsj.com>
2013-10-21 00:50:49 -0400

    Initial Commit

---
+++ public/js/main.js
@@ -0,0 +1,42 @@
+/*
+
+Sniff out new letter vs old letters... 
+
+Add Delete... 
+
+Identify Users by css class... Attribute
+
+Styling...
+
+*/
+
+$(document).ready(function(){
+
+	var socket = io.connect('http://localhost:8080');
+
+	socket.emit('requestAllLetters', function(data){
+		console.log("Done with gett al letters");
+	})
+
+	socket.on('getAllLetters',function (data) {
+		console.log(data);
+		console.log(data.length);
+		$('#letters').html('');
+		for(i in data){
+			$('#letters').append('<div class="letter">' + data[i].letter + '</div>')
+		}
+	});
+
+	$(document).keypress(function(e){
+		var letter = String.fromCharCode(e.keyCode)
+		if(typeof(letter) == 'string' && letter != ''){
+			socket.emit('inserLetter', { letter: letter });
+		}
+	})
+
+	function getRandomLetter(){
+		var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+		return possible[parseInt(Math.random() * possible.length)];
+	}
+
+});
---
+++ server.js
@@ -0,0 +1,190 @@
+var express = require('express');
+var http = require('http');
+var mysql = require("mysql"); 
+var app = require('express')();
+var server = require('http').createServer(app)
+var io = require('socket.io').listen(server);
+
+// Create the connection. 
+// Data is default to new mysql installation and should be changed according to your configuration. 
+var connection = mysql.createConnection({ user: "root", password: "ursulita", database: "thejsj_node_test" }); 
+
+server.listen(8080);
+
+app.use(express.bodyParser());
+
+app.configure(function(){
+  app.use('/media', express.static(__dirname + '/media'));
+  app.use(express.static(__dirname + '/public'));
+});
+
+/* --------------------
+
+	Utilities
+
+-------------------- */
+
+// Get Letters
+app.get('/api/letters/', function(req, res){
+	getAllLetters(function(data){
+		console.log(data);
+		// Parse into a json
+	});
+});
+
+// Insert Letter
+app.post('/api/insert/', function(req, res){
+	insertLetterIntoDatabase(req.body.letter);
+	// Parse Response into JSON
+});
+
+app.get('/', function(req, res){
+	res.sendfile('public/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+
+	socket.on('requestAllLetters', function(){
+		getAllLetters(function(data){
+			socket.emit('getAllLetters', data);
+		});
+	});
+
+  	socket.on('inserLetter', function (letter) {
+		insertLetter(letter, function(data){
+			getAllLetters(function(data){
+				socket.emit('getAllLetters', data); 
+			});
+		})
+	});
+});
+
+/* --------------------
+
+	MySql Functions
+
+-------------------- */
+
+function insertLetter(letter, callback){
+	getNetworkIPs(function (error, ip) {
+		var location = get_location('api.hostip.info', '/get_json.php?ip=' + ip[0], function(resposne){
+			var data = JSON.parse(resposne);
+			console.log(letter.letter);
+			// Build Dict
+			var letter_query  = {
+				letter: letter.letter, 
+				location: data.city + ", " + data.country_code, 
+				ip_address : ip[0]
+			};
+			console.log('letter_query:');
+			console.log(letter_query);
+			var query = connection.query('INSERT INTO letters SET ?', letter_query, function(err, result) {
+				if (err) throw err;
+				console.log('New Row Inserted : ' + result.insertId);
+				query_response = {
+					status : 'succses',
+					letter_id : result.insertId,
+				}
+				console.log("qUERY RESPONSE: ");
+				console.log(JSON.stringify(query_response));
+				callback(JSON.stringify(query_response));
+			});
+		});
+		if (error) {
+			console.log('error:', error);
+		}
+	}, false);
+}
+
+function getAllLetters(callback){
+	connection.query('SELECT * FROM letters;', function (error, rows, fields) { 
+		var new_dict = {}; 
+		for(i in rows){
+			new_dict[rows[i].letter_id] = rows[i];
+		}
+		callback(new_dict); 
+	});
+}
+
+/* --------------------
+
+	Utilities
+
+-------------------- */
+
+var getNetworkIPs = (function () {
+    var ignoreRE = /^(127\.0\.0\.1|::1|fe80(:1)?::1(%.*)?)$/i;
+
+    var exec = require('child_process').exec;
+    var cached;
+    var command;
+    var filterRE;
+
+    switch (process.platform) {
+    case 'win32':
+    //case 'win64': // TODO: test
+        command = 'ipconfig';
+        filterRE = /\bIPv[46][^:\r\n]+:\s*([^\s]+)/g;
+        break;
+    case 'darwin':
+        command = 'ifconfig';
+        filterRE = /\binet\s+([^\s]+)/g;
+        // filterRE = /\binet6\s+([^\s]+)/g; // IPv6
+        break;
+    default:
+        command = 'ifconfig';
+        filterRE = /\binet\b[^:]+:\s*([^\s]+)/g;
+        // filterRE = /\binet6[^:]+:\s*([^\s]+)/g; // IPv6
+        break;
+    }
+
+    return function (callback, bypassCache) {
+        if (cached && !bypassCache) {
+            callback(null, cached);
+            return;
+        }
+        // system call
+        exec(command, function (error, stdout, sterr) {
+            cached = [];
+            var ip;
+            var matches = stdout.match(filterRE) || [];
+            //if (!error) {
+            for (var i = 0; i < matches.length; i++) {
+                ip = matches[i].replace(filterRE, '$1')
+                if (!ignoreRE.test(ip)) {
+                    cached.push(ip);
+                }
+            }
+            //}
+            callback(error, cached);
+        });
+    };
+})();
+
+
+function get_location(host, path, this_calback){
+	//The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+	console.log('HOST : ' + host);
+	console.log('PATH : ' + path);
+	var options = {
+		host: host,
+		path: path,
+	};
+
+	callback = function(response) {
+		var str = '';
+
+		//another chunk of data has been recieved, so append it to `str`
+		response.on('data', function (chunk) {
+			str += chunk;
+		});
+
+		//the whole response has been recieved, so we just print it out here
+		response.on('end', function () {
+			console.log(str);
+			this_calback(str);
+		});
+	}
+
+	http.request(options, callback).end();
+}
