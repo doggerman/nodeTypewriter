fe42dfc85cd4d3c24b4023c164887f3c23d18467
thejsj <jorge.silva@thejsj.com>
2013-10-22 00:09:14 -0400

    Adding User Classes

--- public/js/main.js
+++ public/js/main.js
@@ -21,7 +21,9 @@ $(document).ready(function(){
 	socket.on('getAllLetters',function (data) {
 		$('#letters').html('');
 		for(i in data){
-			$('#letters').append('<div class="letter ' + data[i].ip_address + '">' + data[i].letter + '</div>')
+			var key = data[i].ip_address.substring(0,6);
+			apppendCssClass(key);
+			$('#letters').append('<div class="letter user_' + key + '">' + data[i].letter + '</div>')
 		}
 	});
 
@@ -35,6 +37,33 @@ $(document).ready(function(){
 	function getRandomLetter(){
 		var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 		return possible[parseInt(Math.random() * possible.length)];
+	}
+
+	function apppendCssClass(key){
+		var style = document.createElement('style');
+		style.type = 'text/css';
+		/*
+		border: solid 1px green;
+		background: rgba(0, 128, 0, 0.2);
+		text-shadow: 0px 0px 2px rgba(0, 128, 0, 0.5);
+		color: green;
+		*/
+		var rgb = hexToRgb(key);
+		style.innerHTML = '.user_' + key + ' { \
+			color: #'+key+';\
+			background: rgba( ' + rgb.r + ', ' + rgb.g + ', ' + rgb.b + ', 0.2);\
+			border-color: #'+ key +'; \
+			text-shadow: 0px 0px 2px rgba( ' + rgb.r + ', ' + rgb.g + ', ' + rgb.b + ', 0.5);
+		}';
+		document.getElementsByTagName('head')[0].appendChild(style);
+	}
+	function hexToRgb(hex) {
+		var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+		return result ? {
+			r: parseInt(result[1], 16),
+			g: parseInt(result[2], 16),
+			b: parseInt(result[3], 16)
+		} : null;
 	}
 
 });
