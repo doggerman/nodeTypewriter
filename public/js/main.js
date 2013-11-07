d51a3e50986f2c65a229171f2ca2542718bacb7b
thejsj <jorge.silva@thejsj.com>
2013-11-07 15:22:57 -0500

    Configuring Sockets

--- public/js/main.js
+++ public/js/main.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
 		console.log("host : ");
 		console.log(host);
 		//var socket = io.connect(host);
-		socket = io.connect('http://thejsj.webfactional.com:27862');
+		socket = io.connect('ws://thejsj.webfactional.com:27862');
 		console.log(" + Socket: ");
 		console.log(socket);
 		// var socket = new io.Socket();
