94eb30e13174256372963a0af3ce912b547fc4f4
thejsj <jorge.silva@thejsj.com>
2013-11-09 18:54:31 -0500

    Showing IP addres to the client

--- server.js
+++ server.js
@@ -144,7 +144,8 @@ io.sockets.on('connection', function (socket) {
 	console.log('Socket connection : ' + ip_address);
     var encrypted_ip_address = crypto.createHash('md5').update(ip_address).digest("hex");
    	if(session_debug){ console.log('Encrypted Ip Address : ' + encrypted_ip_address); }
-    socket.emit('getIpAddressRaw', ip_address);
+
+    socket.emit('getIpAddressRaw', [ip_address, socket.handshake.address.address]);
     socket.emit('getIpAddress', encrypted_ip_address);
 
 	socket.on('init',function(eia){
