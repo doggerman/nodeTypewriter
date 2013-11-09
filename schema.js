c2ac3a06c61bc6b88f880a58bcbb1c33e7034554
thejsj <jorge.silva@thejsj.com>
2013-11-09 15:13:30 -0500

    Changing Database Schema generation script

--- schema.js
+++ schema.js
@@ -7,19 +7,24 @@ var pg = require('pg').native
 client = new pg.Client(conString);
 client.connect();
 
-var insert_letters_table = 'create table IF NOT EXISTS letters ( \
-	id SERIAL PRIMARY KEY,\
-	letter VARCHAR(2), \
-	user_id INTEGER,
-	created TIMESTAMP DEFAULT current_timestamp \
-	);';
+var insert_letters_table = [
+	'create table IF NOT EXISTS letters ( ',
+	'id SERIAL PRIMARY KEY, ',
+	'letter VARCHAR(2), ',
+	'user_id INTEGER, ',
+	'created TIMESTAMP DEFAULT current_timestamp ',
+	'');
+insert_letters_table = insert_letters_table.join();
 
-var insert_users_table = 'create table IF NOT EXISTS users ( \
-	id SERIAL PRIMARY KEY, \
-	color VARCHAR(255), \
-	ip_address VARCHAR(255), \
-	location VARCHAR(255)\
-	);';
+var insert_users_table = [
+	'create table IF NOT EXISTS users ( ',
+	'id SERIAL PRIMARY KEY, ',
+	'color VARCHAR(255), ',
+	'ip_address VARCHAR(255), ',
+	'location VARCHAR(255) ',
+	');'
+];
+insert_users_table = insert_users_table.join();
 
 var drop_tables = 'DROP TABLE IF EXISTS users, letters CASCADE;'
 
